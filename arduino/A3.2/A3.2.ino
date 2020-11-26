7930956ace5b264ce6b84fd785132010224db40c
Alfredopflc <jose.venegas17@tectijuana.edu.mx>
2020-11-26 13:40:54 -0800

    Cambio 22

--- arduino/A3.2/A3.2.ino
+++ arduino/A3.2/A3.2.ino
@@ -3,18 +3,19 @@ const int touchPin = 4;
 const int ledPin = 17;
 //Cambiar con el valor umbral
 const int threshold = 20;
+
 //Variable para almacenar el valor del pin touch
 int touchValue;
 const int freq = 5000;
 const int ledChannel = 0;
 const int resolution = 8;
 int valor = 0;
+bool estado = false;
 
 void setup() {
   //Iniciar comunicacion con el monitor en serie
   Serial.begin(115200);
   delay(1000);
-  //Asignamos el LED como salida
  
   ledcSetup(ledChannel, freq, resolution);
   //Aqui se declara el ledPin para ser controlado
@@ -23,28 +24,38 @@ void setup() {
 
 void loop() {
   //Lee el estado del pin touch
+  touchValue = 21;
   touchValue = touchRead(touchPin);
   //Se imprime el valor obtenido del pin touch
-  Serial.print(touchValue);
+  Serial.println(touchValue);
 
-    if(touchValue < threshold){
+  //Si esta apagado, se prende el LED poco a poco
+    if((touchValue < threshold) && (estado == false)){  
       for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){
-    //se cambia el brillo del LED segun el valor de dutyCycle
-    ledcWrite(ledChannel, dutyCycle);
-    delay(25);
-      }            
+      ledcWrite(ledChannel, dutyCycle);
+      delay(25);
+      }   
+            
     Serial.println(" - LED on");
+    estado = true;
+    touchValue = 21;
+    delay(1000);
     }
 
-  //Si no, entonces el LED se apaga e imrpime "LED apagado"
-  else{
+  //Si esta prendido, el led apagara, prendera y apagara    
+  if((touchValue < 20) && (estado == true)){
     ledcWrite(ledChannel, 0);
     delay(1000);
     ledcWrite(ledChannel, 255);
     delay(1000);
     ledcWrite(ledChannel, 0);
     delay(1000);
-  }
+    
+    Serial.println(" - LED off");
+    estado = false;
+    touchValue = 21;
+    delay(1000);
+  } 
 
  delay(500);
 }
