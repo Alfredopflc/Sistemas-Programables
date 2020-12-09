4e747d9a66d8fe2a327e8eb2ee85861ef2ebfd0c
Alfredopflc <jose.venegas17@tectijuana.edu.mx>
2020-12-09 14:14:04 -0800

    Cambio 26

---
+++ arduino/C4.2_demo/C4.2_demo.ino
@@ -0,0 +1,53 @@
+/*
+ WiFi Web Server Simple
+ */
+
+#include <WiFi.h>
+#include <WebServer.h>
+
+const char* ssid = "CASA-2.4";
+const char* password =  "abcde12345";
+
+WebServer server(80);  // Object of WebServer(HTTP port, 80 is defult)
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Try Connecting to ");
+  Serial.println(ssid);
+
+  // Connect to your wi-fi modem
+  WiFi.begin(ssid, password);
+
+  // Check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected successfully");
+  Serial.print("Got IP: ");
+  Serial.println(WiFi.localIP());  //Show ESP32 IP on serial
+
+  server.on("/", handle_root);
+
+  server.begin();
+  Serial.println("HTTP server started");
+  delay(100); 
+}
+
+void loop() {
+  server.handleClient();
+}
+
+// HTML & CSS contents which display on web server
+String HTML = "<!DOCTYPE html>\
+<html>\
+<body>\
+<h1>Mi Primer Servidor Web with ESP32 - Station Mode &#128522;</h1>\
+</body>\
+</html>";
+
+// Handle root url (/)
+void handle_root() {
+  server.send(200, "text/html", HTML);
+}
