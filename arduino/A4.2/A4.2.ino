8b18daf758d4b22fb50db1d1fb0362e0539da1e5
Alfredopflc <jose.venegas17@tectijuana.edu.mx>
2020-12-14 15:06:54 -0800

    Cambio 27

--- arduino/A4.2/A4.2.ino
+++ arduino/A4.2/A4.2.ino
@@ -1,136 +1,23 @@
-// Libreria de WiFi
-#include <WiFi.h>
-
-// SSID y contraseña
-const char* ssid = "CASA-2.4";
-const char* password =  "abcde12345";
-
-// Establecer el número de puerto del servidor web en 80
-WiFiServer server(80);
-
-// Variable para almacenar la solicitud HTTP
-String header;
-
-// Variables auxiliares para almacenar el estado de salida actual
-String output26State = "off";
-String output27State = "off";
-
-// Salidas GPIO
-const int output26 = 26;
-//const int output27 = 27;
-
-// Tiempo actual
-unsigned long currentTime = millis();
-// Tiempo previo
-unsigned long previousTime = 0; 
-// Tiempo de espera
-const long timeoutTime = 2000;
+int LDR = 34; 
+int LDR_val = 0;
+int relay = 16; 
 
 void setup() {
-  Serial.begin(115200);
-  // Declarar variables como salida
-  pinMode(output26, OUTPUT);
-//  pinMode(output27, OUTPUT);
- 
-  digitalWrite(output26, LOW);
-//  digitalWrite(output27, LOW);
-
-  // Conectarse al WiFi con SSID y contraseña
-  Serial.print("Conectando a: ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  // Se imprime IP
-  Serial.println("");
-  Serial.println("WiFi conectado");
-  Serial.println("Direccion IP: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
+  Serial.begin(9600);
+  pinMode(relay,OUTPUT);
 }
 
-void loop(){
-  WiFiClient client = server.available();   // Clientes entrantes
+void loop() {
+  LDR_val = analogRead(LDR);
+  Serial.println(LDR_val);
 
-  if (client) {                             // Si un nuevo cliente se conecta,
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");          // imprime un mensaje en el puerto serie
-    String currentLine = "";                // hacer una cadena para contener los datos entrantes del cliente
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {
-      currentTime = millis();
-      if (client.available()) {             // Si hay bytes para leer del cliente,
-        char c = client.read();             // leer un byte, luego
-        Serial.write(c);                    // imprímir en el monitor de serie
-        header += c;
-        if (c == '\n') {                    // si el byte es un carácter de nueva línea
-          // si la línea actual está en blanco, tiene dos caracteres de nueva línea seguidos.
-          // ese es el final de la solicitud HTTP del cliente, así que envíe una respuesta:
-          
-          if (currentLine.length() == 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // enciende y apaga el LED
-            if (header.indexOf("GET /26/on") >= 0) 
-            {
-              Serial.println("GPIO 26 on");
-              output26State = "Encendido";
-              digitalWrite(output26, HIGH);
-            } 
-            
-            else if (header.indexOf("GET /26/off") >= 0) 
-            {
-              Serial.println("GPIO 26 off");
-              output26State = "Apagado";
-              digitalWrite(output26, LOW);
-            } 
-            
-            // Mostrar la página web HTML
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            
-            // CSS para diseñar los botones de encendido / apagado
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-            
-            // Encabezado de la página web
-            client.println("<body><h1>ESP32 Servidor Web</h1>");
-            
-            // Muestra el estado actual y los botones ON / OFF para GPIO 26
-            client.println("<p>GPIO 26 - ESTADO: " + output26State + "</p>");
-            // Si output26State está apagado, muestra el botón ON     
-            if (output26State=="Apagado") {
-              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-
-            client.println("</body></html>");
-            // La respuesta HTTP termina con otra línea en blanco
-            client.println();
-            // Salir del bucle while
-            break;
-          } else { // si tiene una nueva línea, borre currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  
-          currentLine += c;  
-        }
-      }
-    }
-    // Borrar la variable de encabezado
-    header = "";
-    // Cerrar la conexión
-    client.stop();
-    Serial.println("Cliente desconectado.");
-    Serial.println("");
+  if(LDR_val >= 500){
+    digitalWrite(relay,LOW);
   }
+
+   else{
+    digitalWrite(relay,HIGH);
+  }
+  
+  delay(1000);
 }
--- arduino/C4.2_demo/C4.2_demo.ino
+++ arduino/C4.2_demo/C4.2_demo.ino
@@ -1,44 +1,39 @@
 /*
- WiFi Web Server Simple
- */
-
+WiFi Web Server Simple
+*/
 #include <WiFi.h>
 #include <WebServer.h>
-
-const char* ssid = "CASA-2.4";
-const char* password =  "abcde12345";
-
-WebServer server(80);  // Object of WebServer(HTTP port, 80 is defult)
-
+const char* ssid = "<identificador>";
+const char* password = "<password>";
+WebServer server(80); // Object of WebServer(HTTP port, 80 is defult)
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Try Connecting to ");
-  Serial.println(ssid);
-
-  // Connect to your wi-fi modem
-  WiFi.begin(ssid, password);
-
-  // Check wi-fi is connected to wi-fi network
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected successfully");
-  Serial.print("Got IP: ");
-  Serial.println(WiFi.localIP());  //Show ESP32 IP on serial
-
-  server.on("/", handle_root);
-
-  server.begin();
-  Serial.println("HTTP server started");
-  delay(100); 
+Serial.begin(115200);
+Serial.println("Try Connecting to ");
+Serial.println(ssid);
+// Connect to your wi-fi modem
+WiFi.begin(ssid, password);
+// Check wi-fi is connected to wi-fi network
+while (WiFi.status() != WL_CONNECTED) {
+delay(1000);
+Serial.print(".");
 }
+Serial.println("");
+Serial.println("WiFi connected successfully");
+Serial.print("Got IP: ");
+Serial.println(WiFi.localIP()); //Show ESP32 IP on serial
 
+C4.2_JoseAlfredoVenegasMedina_Verde.md 9/12/2020
+
+3 / 10
+
+server.on("/", handle_root);
+server.begin();
+Serial.println("HTTP server started");
+delay(100);
+}
 void loop() {
-  server.handleClient();
+server.handleClient();
 }
-
 // HTML & CSS contents which display on web server
 String HTML = "<!DOCTYPE html>\
 <html>\
@@ -46,8 +41,7 @@ String HTML = "<!DOCTYPE html>\
 <h1>Mi Primer Servidor Web with ESP32 - Station Mode &#128522;</h1>\
 </body>\
 </html>";
-
 // Handle root url (/)
 void handle_root() {
-  server.send(200, "text/html", HTML);
+server.send(200, "text/html", HTML);
 }
