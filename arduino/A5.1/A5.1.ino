bf77b7fb95cb7429fde8f204de4966b6d20cce83
Alfredopflc <jose.venegas17@tectijuana.edu.mx>
2021-01-13 13:54:37 -0800

    Cambio 29

--- arduino/A5.1/A5.1.ino
+++ arduino/A5.1/A5.1.ino
@@ -11,8 +11,10 @@
 //      5V        VCC
 //      GND       GND
 
+//Objeto servo
 Servo servo1;
 
+//Declarar salidas
 #define S0 27
 #define S1 26
 #define S2 25
@@ -20,11 +22,13 @@ Servo servo1;
 #define sensorSalida 32
 #define servoPin 13
 
+//Variables RGB
 int Rojo_Frec = 0;
 int Verde_Frec = 0;
 int Azul_Frec = 0;
 
 void setup() {
+  //Pines RGB
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
@@ -39,67 +43,48 @@ void setup() {
   Serial.begin(9600);
 }
 void loop() {
-                          // Configura el filtor ROJO para tomar lectura
+// Configura el filtro ROJO para tomar lectura
   digitalWrite(S2,LOW);
   digitalWrite(S3,LOW);
   delay(100);
   Rojo_Frec= pulseIn(sensorSalida, LOW);
-  Serial.print(" R= "); Serial.print(Rojo_Frec);
+//  Serial.print(" R= "); Serial.print(Rojo_Frec);
   delay(100);
-                            // Configura el filtor VERDE para tomar lectura
+  
+// Configura el filtro VERDE para tomar lectura
   digitalWrite(S2,HIGH);
   digitalWrite(S3,HIGH);
   delay(100);
   Verde_Frec = pulseIn(sensorSalida, LOW);
-  Serial.print(" V= "); Serial.print(Verde_Frec);
+ // Serial.print(" V= "); Serial.print(Verde_Frec);
   delay(100);
-                           // Configura el filtor AZUL para tomar lectura
+  
+// Configura el filtro AZUL para tomar lectura
   digitalWrite(S2,LOW);
   digitalWrite(S3,HIGH);
   delay(100);
   Azul_Frec = pulseIn(sensorSalida, LOW);
-  Serial.print(" A= "); Serial.print(Azul_Frec);
+ // Serial.print(" A= "); Serial.print(Azul_Frec);
   delay(100);
 
-                  //Valores obtenidos en la calibracion del sensor, son propios de cada usuario 
- /*                 
- if (Rojo_Frec < 85 && Verde_Frec > 80 && Azul_Frec > 80)  Serial.print(" . *** ROJO **"); servo1.write(0);
- if (Rojo_Frec > 80 && Verde_Frec > 100 && Azul_Frec < 80)  Serial.print(" . *** AZUL **"); servo1.write(45);
- if (Rojo_Frec > 185 && Verde_Frec < 140 && Azul_Frec < 130)  Serial.print(" . *** VERDE **"); servo1.write(90);
- if (Rojo_Frec < 35 && Verde_Frec < 35 && Azul_Frec < 35)  Serial.print(" . *** BLANCO **"); servo1.write(135);
- if (Rojo_Frec > 130 && Verde_Frec > 200 && Azul_Frec >150)  Serial.print(" . *** NEGRO **"); servo1.write(180);
- */
-
-if ( 460 > Rojo_Frec > 435 && 500 > Verde_Frec > 470 && 420 >Azul_Frec > 400)
-{  
-  Serial.println(" . *** NEGRO **");
-  servo1.write(0); 
+//Rango de colores
+if (Rojo_Frec < 100 && Verde_Frec > 250 && Azul_Frec > 200) //Rojo
+{
+  Serial.println(2); 
+  servo1.write(0);  
 }
 
-if (80 > Rojo_Frec > 70 && 280 > Verde_Frec > 265 && 220 > Azul_Frec > 200)
+else if (Rojo_Frec > 270 && Verde_Frec < 200 && Azul_Frec < 100) //Azul
 {
-  Serial.println(" . *** ROJO **"); 
-  servo1.write(45);  
-}
-
-if (280 > Rojo_Frec > 270 && 136 > Verde_Frec > 125 && 80 > Azul_Frec > 75)
-{
-  Serial.println(" . *** AZUL **"); 
+  Serial.println(1); 
   servo1.write(90);
 }
 
-if (50 > Rojo_Frec > 40 && 55 > Verde_Frec > 45 && 50 > Azul_Frec > 40)
+else if (Rojo_Frec > 280 && Verde_Frec < 250 && Azul_Frec < 300) //Verde
 {
-  Serial.println(" . *** BLANCO **"); 
-  servo1.write(135);  
-}
-
-if (295 > Rojo_Frec > 285 && 190 > Verde_Frec > 180 && 250 > Azul_Frec > 240)
-{
-  Serial.println(" . *** VERDE **"); 
+  Serial.println(0); 
   servo1.write(180);  
 }
 
- Serial.println("*");
  delay(200);
 }
